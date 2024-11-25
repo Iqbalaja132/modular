fc8f7b7aae57d7965f54e54dcb16b8ec563f8c41
Iqbalaja132 <iqbalmuhamadramdhan0@gmail.com>
2024-11-25 11:54:16 +0700

    tambahan lagi

--- views/hasil_form.php
+++ views/hasil_form.php
@@ -4,7 +4,12 @@
         $username = $_POST['username'];
         $email = $_POST['email'];
         $password = $_POST['password'];
+        $nama = $_POST['nama_user'];
         $alamat = $_POST['alamat_user'];
+        $jk = $_POST['jenis_kelamin'];
+        $tempatlahir = $_POST['tempatlahir_user'];
+        $tanggallahir = $_POST['tanggallahir_user'];
+
 
         echo "Username : " . $username;
         echo "<br>";
@@ -12,6 +17,13 @@
         echo "<br>";
         echo "Password : " . $password;
         echo "<br>";
+        echo "Nama : " . $nama;
+        echo "<br>";
         echo "Alamat : " . $alamat;
-
+        echo "<br>";
+        echo "Jenis kelamin : " . $jk;
+        echo "<br>";
+        echo "Tempat Lahir : " . $tempatlahir;
+        echo "<br>";
+        echo "Tanggal Lahir : " . $tanggallahir;
         ?>
